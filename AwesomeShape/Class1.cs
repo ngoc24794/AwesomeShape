f4172489225aa585ab91b7e706e25c42839bcd34
Ngọc Nguyễn <ngoc24794@gmail.com>
2018-08-24 00:16:15 +0700

    thêm constructor cho Class1

--- AwesomeShape/Class1.cs
+++ AwesomeShape/Class1.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AwesomeShape
+﻿namespace AwesomeShape
 {
     public class Class1
     {
+        public Class1() { }
     }
 }
